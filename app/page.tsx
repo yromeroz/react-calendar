758fd718f8a0866cb3752216259ccedb9ad9cae5
Yidier <yidier@gmail.com>
2025-11-08 15:39:39 -0500

    update name param

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ const getEventsData = async () => {
     const reservas = await db
       .select({
         id: reservaTable.id,
-        title: reservaTable.name,
+        name: reservaTable.name,
         date: reservaTable.time,
         endTime: reservaTable.endTime,
         description: reservaTable.description,
@@ -45,7 +45,7 @@ const getEventsData = async () => {
       .groupBy(reservaTable.id);
     return reservas.map((reserva) => ({
       id: Number(reserva.id),
-      title: reserva.title,
+      name: reserva.name,
       date: dayjs(reserva.date).toISOString(),
       endTime: dayjs(reserva.endTime).toISOString(),
       courseId: Number(reserva.courseId),
--- components/event-renderer.tsx
+++ components/event-renderer.tsx
@@ -35,7 +35,7 @@ export function EventRenderer({ date, view, events }: EventRendererProps) {
   return (
     <>
       {visibleEvents.map((event) => { 
-        const eventName = event.title !== "" ? event.title : "-";
+        const eventName = event.name !== "" ? event.name : "-";
         const eventColor = event.color !== "" ? event.color : "#98b8ff"; // Default to blue if not found
         const darker = adjustColor(eventColor, 120);
         const lighter = adjustColor(eventColor, 150);
